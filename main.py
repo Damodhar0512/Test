ac00a824151674c6a094ce6048bc0dbc74bea44e
DAMODHAR <ndamoo@gmail.com>
2022-07-18 22:33:26 +0530

    this is my first push file

---
+++ main.py
@@ -0,0 +1,10 @@
+
+import mysql.connector as conn
+
+mydb = conn.connect(host = "localhost" , user ="root" , passwd = "Newyear@2022" )
+cursor = mydb.cursor()
+cursor.execute("create database damoo")
+cursor.execute("show databases")
+cursor.execute("create table damoo.dbtable1(employe_id int(10)  , emp_name varchar(80) , emp_mailid varchar(20),emp_salary int(6)) , emp_attendence int(3)")
+# print(cursor.fetchall())
+#
